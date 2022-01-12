1234a82c176b304fe6d3f5475710bd49bbe05eeb
Yulia Gavrisheva <91274410+YuliaGavrisheva@users.noreply.github.com>
2022-01-12 13:34:43 +0300

    Add files via upload

---
+++ bot/api/static_text.py
@@ -0,0 +1,40 @@
+START = """
+Привет, %s! \nЗагрузи фото и определи, что за кроссовок?
+Список доступных команд:
+/start - Запуск/рестарт бота.
+/about - Краткая информация.
+"""
+ABOUT = """ 
+Бот обучен на 20 классах, а именно: 
+Adidas NMD R1
+Adidas Ultra Boost 4.0 
+Adidas Yeezy Boost 350 
+Adidas Yeezy Boost 700 
+Air Jordan 1 Retro High 
+Air Jordan 4 
+Air Jordan 6 
+Air Jordan 11 
+Air Jordan 12
+Air Jordan 13
+Nike Air Force 1 Low
+Nike Air Huarache
+Nike Air Max 1
+Nike Air Max 90
+Nike Air Max 95
+Nike Air VaporMax
+Nike Blazer
+Nike Dunk low
+Reebok Instapump Fury
+Unknown(Неопределённый класс)
+"""
+STICKER1 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER2 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER3 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER4 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER5 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER6 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER7 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER8 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER9 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+STICKER10 = 'CAACAgIAAxkBAAEDH3ZhcPwuk8_ea46pVXd7kcKtuJaJCgACeSUAAp7OCwABlPU3foy-CJwhBA'
+
